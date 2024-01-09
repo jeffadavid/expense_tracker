c4404240d8411ef2b71c173e9a5d60f33adc2e4d
jeffadavid <jeffadavid@gmail.com>
2024-01-09 16:41:13 +0300

    Added the Expensedate and expenses components, put code from App.js to the expense component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
@@ -28,26 +28,7 @@ function App() {
   return (
     <div>
       <h2>expense tracker</h2>
-      <ExpenseItem 
-        date={expenses[0].date} 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-      />
-      <ExpenseItem 
-        date={expenses[1].date} 
-        title={expenses[1].title} 
-        amount={expenses[1].amount} 
-      />
-      <ExpenseItem 
-        date={expenses[2].date} 
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-      />
-      <ExpenseItem 
-        date={expenses[3].date} 
-        title={expenses[3].title} 
-        amount={expenses[3].amount} 
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
